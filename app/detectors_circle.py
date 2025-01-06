43d6057e1ae89b27909669fb5ef4db1ea3ed7ef5
Константин <konstantin.mazurenkov@lp.lukoil.com>
2025-01-06 15:03:52 +0500

    add utils

--- app/detectors_circle.py
+++ app/detectors_circle.py
@@ -3,13 +3,19 @@ from datetime import datetime
 from observer.observer import Subject
 import cv2
 import numpy as np
+import pytz
+import time
+
+# Получаем время в Московском часовом поясе
+moscow_tz = pytz.timezone('Europe/Moscow')
 
 class CircleDetector(Subject):
     def __init__(
             self, repository: MovementRepository, 
             dp=0.5, min_dist=200, 
             param1=100, param2=150, 
-            min_radius=70, max_radius=10000
+            min_radius=70, max_radius=10000,
+            min_delay=10.0
         ):
         super().__init__()
         self.dp = dp  # Разрешение активации в пространстве параметров
@@ -22,8 +28,11 @@ class CircleDetector(Subject):
         self.min_radius = min_radius  # Минимальный радиус кругов
         self.max_radius = max_radius  # Максимальный радиус кругов
         self.repository = repository
+        self.last_detection_time = 0  # Время последнего обнаружения
+        self.min_delay = min_delay  
 
     def process_frame(self, frame):
+        current_time = time.time()  # Получаем текущее время
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Применение медианного фильтра для уменьшения шума
         gray = cv2.medianBlur(gray, 5) 
@@ -39,14 +48,20 @@ class CircleDetector(Subject):
         )
         # Если круги найдены, обрабатываем их
         if circles is not None:
-            for (x, y, radius) in np.uint16(np.around(circles[0, :])):
-                # Рисуем окружность и центр
-                cv2.circle(frame, (x, y), radius, (0, 255, 0), 3)  
-                cv2.circle(frame, (x, y), 2, (0, 0, 255), 3)       
-            message = "Circle detected in framee"
-            self.notify(message)
-            movement = Movement(timestamp=datetime.utcnow(), description=message)
-            self.repository.add_movement(movement)        
+            # Проверяем время последнего обнаружения
+            if (current_time - self.last_detection_time) >= self.min_delay:
+                for (x, y, radius) in np.uint16(np.around(circles[0, :])):
+                    cv2.circle(frame, (x, y), radius, (0, 255, 0), 3)  
+                    cv2.circle(frame, (x, y), 2, (0, 0, 255), 3)       
+                message = "Circle detected in frame"
+                self.notify(message)
+                movement = Movement(
+                    timestamp=datetime.now(moscow_tz), 
+                    description=message
+                )
+                self.repository.add_movement(movement)
+                # Обновляем время последнего обнаружения
+                self.last_detection_time = current_time          
         return frame
 
 '''
@@ -64,6 +79,7 @@ class CircleDetector(Subject):
    порог для обнаруженных кругов.
    - min_radius и max_radius: Минимальный и максимальный радиусы 
    кругов для обнаружения (min_radius=70 - детектирует круг 0,15м на расстоянии 1,3м).
+   - min_delay: минимальная задержка между детектированиями в секундах
 
 2. Медианный фильтр:
    - Обработанный кадр сначала преобразуется в оттенки серого 
